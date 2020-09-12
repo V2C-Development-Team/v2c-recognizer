7996dcdaae9558490ea083efcdb5c5499ad25b01
Rashed Alrashed <ralrashed@uco.edu>
2020-09-11 20:12:50 -0500

    Made a SpeechToText() Function

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # v2c-recognizer
 V2C voice recognition module
-test
+
+recource: https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
+Python requirments
+- python3.6
+- pip3.6
+
+pip commands for windows:
+    pip install speechrecognition
+    pip install pyaudio
--- Recognizer/speech.py
+++ Recognizer/speech.py
@@ -1,50 +1,58 @@
-# Python program to translate 
-# speech to text and text to speech 
-  
-  
-import speech_recognition as sr 
+# Python program to translate
+# speech to text and text to speech
 
-  
-# Initialize the recognizer  
-r = sr.Recognizer()  
-    
-# Loop infinitely for user to 
-# speak 
-  
-while(1):     
-      
-    # Exception handling to handle 
-    # exceptions at the runtime 
-    try: 
-          
-        # use the microphone as source for input. 
-        with sr.Microphone() as source2: 
-              
-            # wait for a second to let the recognizer 
-            # adjust the energy threshold based on 
-            # the surrounding noise level  
-            r.adjust_for_ambient_noise(source2, duration=0.2) 
-            
-            print('Say: ')
-            #listens for the user's input  
-            audio2 = r.listen(source2) 
-              
-            # Using google to recognize audio 
-            MyText = r.recognize_google(audio2) 
-            MyText = MyText.lower() 
 
-            
-            print("-> "+MyText)
+import speech_recognition as sr
 
-            if MyText == 'hey google':
-                print('activation phrase')
 
-            if MyText == 'exit':
-                exit()
-            #SpeakText(MyText) 
-              
-    except sr.RequestError as e: 
-        print("Could not request results; {0}".format(e)) 
-          
-    except sr.UnknownValueError: 
-        print("unknown error occured") 
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# uses
+
+
+def SpeechToText():
+    # use the microphone as source for input.
+    with sr.Microphone() as source2:
+
+        # wait for a second to let the recognizer
+        # adjust the energy threshold based on
+        # the surrounding noise level
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+
+        print('Say: ')
+        # listens for the user's input
+        audio = r.listen(source2)
+
+        # Using google to recognize audio
+        text = r.recognize_google(audio)
+        # converts all text to lower case
+        text = text.lower()
+        return text
+
+
+# Loop infinitely for user to
+# speak
+
+while(1):
+
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+
+        MyText = SpeechToText()
+
+        print("-> "+MyText)
+
+        if MyText == 'hey google':
+            print('activation phrase')
+
+        if MyText == 'exit':
+            exit()
+        # SpeakText(MyText)
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occured")
