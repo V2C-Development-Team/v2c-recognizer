c93691ba33904121775c18855931d289d1b356b5
Rashed Alrashed <ralrashed@uco.edu>
2020-09-21 02:11:31 -0500

    Added JSON, changed send address

--- Recognizer/speech.py
+++ Recognizer/speech.py
@@ -9,17 +9,20 @@ import asyncio
 import websockets
 import threading
 from _thread import start_new_thread
+import json
 
 # Sends string to websocket
-async def send(text):
-    uri = "ws://127.0.0.1:5698/"
+async def send(command):
+    uri = "ws://127.0.0.1:2585/v1/messages"
     async with websockets.connect(uri) as websocket:
-        await websocket.send(text)
+        await websocket.send(command)
 
 # Initialize the recognizer
 r = sr.Recognizer()
 
 # This function uses the microphone to turn speech to text
+
+
 def SpeechToText():
     # use the microphone as source for input.
     with sr.Microphone() as source2:
@@ -38,6 +41,8 @@ def SpeechToText():
         return text
 
 # This funtion uses a .wav file to turn speech to text
+
+
 def FileToText(file):
     # use the microphone as source for input.
     with sr.AudioFile(file) as source2:
@@ -55,15 +60,23 @@ def FileToText(file):
         text = text.lower()
         return text
 
-#Plays starting sound
+# Plays starting sound
 def playStartSound():
     ps.playsound('start.wav')
-#Plays end sound
+
+# Plays end sound
 def playEndSound():
     ps.playsound('end.wav')
 
-#creating constant threads
 
+# Initializing program
+# Registering recognizer with dispatcher
+register = {
+    "action": "REGISTER_LISTENER",
+    "app": "recognizer",
+    "eavesdrop": False,
+}
+asyncio.get_event_loop().run_until_complete(send(json.dumps(register)))
 
 # Loop infinitely for user to
 # speak
@@ -77,31 +90,41 @@ while(1):
         text = ''
 
         print('Say \"blue\" to input command from microphone, say "file" to input from file')
-        
+
         text = SpeechToText()
 
         print("-> "+text)
 
-        #uses mic after activation phrase
+        # uses mic after activation phrase
         if text == 'blue':
             print('activation phrase')
             playStartSoundThread = threading.Thread(target=playStartSound)
             playStartSoundThread.start()
-            
+
             command = SpeechToText()
             print('Sphnix heard: ' + command)
             playEndSoundThread = threading.Thread(target=playEndSound)
             playEndSoundThread.start()
-            asyncio.get_event_loop().run_in_executor(send(command))
+            command = json.dumps({
+                "action": "DISPATCH_COMMAND",
+                "command": command,
+            })
+            asyncio.get_event_loop().run_until_complete(send(command))
         # uses file to send command
         elif text == 'file':
             # askes user for file name input
             name = input('Input file name: ')
             command = FileToText(name)
-            asyncio.get_event_loop().run_in_executor(send(command))
+            asyncio.get_event_loop().run_until_complete(send(command))
 
         # exit condition
         if text == 'exit':
+            deregister = {
+                "action": "DEREGISTER_LISTENER",
+                "app": "recognizer",
+            }
+            asyncio.get_event_loop().run_until_complete(send(json.dumps(deregister)))
+            time.sleep(0.5)
             exit()
 
     except sr.RequestError as e:
