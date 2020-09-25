e061ad4a90e17bf26c2dbe42b52385238ffa3cf0
Rashed Alrashed <ralrashed@uco.edu>
2020-09-25 02:33:48 -0500

    File to text functionality (#13)
    * Updated read me for file to text requirment
    
    * Changed recognition to use google
    
    * Added FileToText function, terminal user interface
    
    * Removed a comment
    
    * Added websockets, command is sent to websocket
    
    * Changed the interface, multi threaded sound output
    
    * Changed the async send execution function
    
    * Added JSON, changed send address

--- Recognizer/speech.py
+++ Recognizer/speech.py
@@ -1,17 +1,27 @@
 # Python program to translate
-# speech to text and text to speech
-
+# speech to text
+# Sound files such as end.wav and start.wav are from windows system sounds
 
 import speech_recognition as sr
 import playsound as ps
 import time
+import asyncio
+import websockets
+import threading
+from _thread import start_new_thread
+import json
 
+# Sends string to websocket
+async def send(command):
+    uri = "ws://127.0.0.1:2585/v1/messages"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(command)
 
 # Initialize the recognizer
 r = sr.Recognizer()
 
+# This function uses the microphone to turn speech to text
 
-# https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
 
 def SpeechToText():
     # use the microphone as source for input.
@@ -22,16 +32,51 @@ def SpeechToText():
         # the surrounding noise level
         r.adjust_for_ambient_noise(source2, duration=0.2)
 
-        print('Say: !')
         # listens for the user's input
         audio = r.listen(source2)
-        print('worked')
-        # Using sphinx to recognize audio
-        text = r.recognize_sphinx(audio)
+        # Using google to recognize audio
+        text = r.recognize_google(audio)
         # converts all text to lower case
         text = text.lower()
         return text
 
+# This funtion uses a .wav file to turn speech to text
+
+
+def FileToText(file):
+    # use the microphone as source for input.
+    with sr.AudioFile(file) as source2:
+
+        # wait for a second to let the recognizer
+        # adjust the energy threshold based on
+        # the surrounding noise level
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+
+        # listens for the user's input
+        audio = r.record(source2)
+        # Using google to recognize audio
+        text = r.recognize_google(audio)
+        # converts all text to lower case
+        text = text.lower()
+        return text
+
+# Plays starting sound
+def playStartSound():
+    ps.playsound('start.wav')
+
+# Plays end sound
+def playEndSound():
+    ps.playsound('end.wav')
+
+
+# Initializing program
+# Registering recognizer with dispatcher
+register = {
+    "action": "REGISTER_LISTENER",
+    "app": "recognizer",
+    "eavesdrop": False,
+}
+asyncio.get_event_loop().run_until_complete(send(json.dumps(register)))
 
 # Loop infinitely for user to
 # speak
@@ -42,20 +87,44 @@ while(1):
     # exceptions at the runtime
     try:
 
+        text = ''
+
+        print('Say \"blue\" to input command from microphone, say "file" to input from file')
+
         text = SpeechToText()
 
         print("-> "+text)
 
+        # uses mic after activation phrase
         if text == 'blue':
             print('activation phrase')
-            ps.playsound('start.wav')
+            playStartSoundThread = threading.Thread(target=playStartSound)
+            playStartSoundThread.start()
+
             command = SpeechToText()
-            time.sleep(0.1)
             print('Sphnix heard: ' + command)
+            playEndSoundThread = threading.Thread(target=playEndSound)
+            playEndSoundThread.start()
+            command = json.dumps({
+                "action": "DISPATCH_COMMAND",
+                "command": command,
+            })
+            asyncio.get_event_loop().run_until_complete(send(command))
+        # uses file to send command
+        elif text == 'file':
+            # askes user for file name input
+            name = input('Input file name: ')
+            command = FileToText(name)
+            asyncio.get_event_loop().run_until_complete(send(command))
 
-            ps.playsound('end.wav')
-
+        # exit condition
         if text == 'exit':
+            deregister = {
+                "action": "DEREGISTER_LISTENER",
+                "app": "recognizer",
+            }
+            asyncio.get_event_loop().run_until_complete(send(json.dumps(deregister)))
+            time.sleep(0.5)
             exit()
 
     except sr.RequestError as e:
