a27d7df8e219be3974b619dd555fa94882c908a9
Rashed Alrashed <ralrashed@uco.edu>
2020-10-08 21:55:11 -0500

    Added comments and some variable changes

--- Recognizer/speech.py
+++ Recognizer/speech.py
@@ -19,6 +19,7 @@ import keyboard
 # Initialize the recognizer
 r = sr.Recognizer()
 
+#global checks, to pass be
 exitFlag = False
 listening = False
 connected = False
@@ -26,6 +27,8 @@ connected = False
 
 #setting up websocket
 ws = websocket.WebSocket()
+
+# setting up Widget
 widget = tk.Tk()
 widget.winfo_toplevel().title('Recognizer')
 widget.iconbitmap('speaker.ico')
@@ -33,13 +36,16 @@ widget.geometry("300x540")
 widget.lift()
 widget.call('wm', 'attributes', '.', '-topmost', True)
 
+# text on top of the widget
 speakText = tk.Label(text='Say \"blue\" for commands, or alt+ctrl+v')
 speakText.pack()
 
+# mic pitures
 micOffImage = tk.PhotoImage(file='micoff.png')
 micOnImage = tk.PhotoImage(file='micon.png')
 micLabel = tk.Label(widget, image=micOffImage)
 micLabel.pack()
+#speaker pictures
 speakImage = tk.PhotoImage(file='speak.png')
 noSpeakImage = tk.PhotoImage(file='nospeak.png')
 speakLabel = tk.Label(widget, image=noSpeakImage)
@@ -57,14 +63,19 @@ def SpeechToText():
         r.adjust_for_ambient_noise(source2, duration=0.2)
 
         # listens for the user's input
-        audio = r.listen(source2)
-        micLabel.configure(image=micOffImage)
-        micLabel.image = micOffImage
-        # Using google to recognize audio
-        text = r.recognize_google(audio)
-        # converts all text to lower case
-        text = text.lower()
-        return text
+        try:
+            audio = r.listen(source2, timeout = 3)
+            micLabel.configure(image=micOffImage)
+            micLabel.image = micOffImage
+            # Using google to recognize audio
+            text = r.recognize_google(audio)
+            # converts all text to lower case
+            text = text.lower()
+            return text
+        except sr.WaitTimeoutError:
+            micLabel.configure(image=micOffImage)
+            micLabel.image = micOffImage
+            return ''
 
 # This funtion uses a .wav file to turn speech to text
 
@@ -87,19 +98,14 @@ def FileToText(file):
         return text
 
 # Plays starting sound
-
-
 def playStartSound():
     ps.playsound('start.wav')
 
 # Plays ending sound
-
-
 def playEndSound():
     ps.playsound('end.wav')
 
 
-# setting up
 register = {
     "action": "REGISTER_LISTENER",
     "app": "recognizer",
@@ -113,7 +119,7 @@ uri = "ws://127.0.0.1:2585/v1/messages"
 
 
 
-
+# send a command via text
 def SendCommand():
     result = txtCommand.get("1.0", "end")
     payload = {
@@ -123,10 +129,13 @@ def SendCommand():
     }
     ws.send(json.dumps(payload))
 
-
+# Starts the Voice command thread
+# Activates voice command feature with acticvation phrase
 def VoiceCommand():
+    # variables shared between threads
     global listening
     while True:
+        # program end check
         if exitFlag == True:
             exit()
         try:
@@ -143,8 +152,10 @@ def VoiceCommand():
 
             # uses mic after activation phrase
             if text == 'blue' and listening == False:
+                # disables hot key
                 keyboard.remove_hotkey('alt+ctrl+v')
                 listening = True
+                # changes speaker picture to active
                 speakLabel.configure(image=speakImage)
                 speakLabel.image = speakImage
                 print('activation phrase')
@@ -157,16 +168,21 @@ def VoiceCommand():
                 print('Command heard: ' + command)
                 playEndSoundThread = threading.Thread(target=playEndSound)
                 playEndSoundThread.start()
+                # changes speaker picture to inactive
                 speakLabel.configure(image=noSpeakImage)
                 speakLabel.image = noSpeakImage
+                # placed new command in text box
                 txtCommand.delete("1.0", "end")
                 txtCommand.insert("1.0", command)
+                # converts command to JSON
                 command = json.dumps({
                     "action": "DISPATCH_COMMAND",
                     "command": command,
                 })
+                # send command
                 ws.send(command)
                 listening = False
+                # enables hot key
                 keyboard.add_hotkey('alt+ctrl+v', hotKey)
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
@@ -178,81 +194,94 @@ def VoiceCommand():
         except sr.UnknownValueError:
             print("unknown error occured")
 
-
+# hotkey function
 def hotKey():
+    # variables shared between threads
     global listening
+    # diables hotkey
     keyboard.remove_hotkey('alt+ctrl+v')
+    # checks if recognizer is listenning to a command
     if listening == False:
         listening = True
+        # changes speaker picture to active
         speakLabel.configure(image=speakImage)
         speakLabel.image = speakImage
-        print('activation phrase')
         playStartSoundThread = threading.Thread(target=playStartSound)
         playStartSoundThread.start()
-
         command = SpeechToText()
+        # checks if program exited
         if exitFlag == True:
             exit()
         print('Command heard: ' + command)
         playEndSoundThread = threading.Thread(target=playEndSound)
         playEndSoundThread.start()
+        # changes speaker picture to inactive
         speakLabel.configure(image=noSpeakImage)
         speakLabel.image = noSpeakImage
+        # places new command in text box
         txtCommand.delete("1.0", "end")
         txtCommand.insert("1.0", command)
+        # converts command to JSON for the dispatcher
         command = json.dumps({
             "action": "DISPATCH_COMMAND",
             "command": command,
         })
+        # sends command
         ws.send(command)
         listening = False
-        keyboard.add_hotkey('alt+ctrl+v', hotKey)
+    # enables hot key
+    keyboard.add_hotkey('alt+ctrl+v', hotKey)
 
 
 keyboard.add_hotkey('alt+ctrl+v', hotKey)
 
-
+# file to text button function
 def FileToTextButton():
+    # opens file navigator and returns path
     filename = filedialog.askopenfilename(
         initialdir="/", title="Select file", filetypes=[("wav files", "*.wav")])
     print(filename)
+    # converts file to text given the path to the file
     text = FileToText(filename)
+    # prepares command to be sent as JSON
     command = json.dumps({
         "action": "DISPATCH_COMMAND",
         "command": text,
     })
+    # sends command
     ws.send(command)
 
 
-# setting up threads
+# setting up VoiceCommand thread
 voiceCommandThread = threading.Thread(target=VoiceCommand)
 
+# text command textbox
 txtCommand = tk.Text(widget, height=1)
-txtCommand.pack( pady=15, )
+txtCommand.pack( pady=15 )
+# send button for text box
 btnSend = tk.Button(widget, height=1, width=10,
                     text="Send", command=SendCommand)
-
-
 btnSend.pack(pady=15)
 
-
-speakText = tk.Label(text='Select and audio file to become a command')
-speakText.pack(pady = 20)
-
+# audio file text
+audioText = tk.Label(text='Select and audio file to become a command')
+audioText.pack(pady = 20)
+# audio file button
 fileButton = tk.Button(widget, height=1, width=20,
                        text="Select Audiofile", command=FileToTextButton)
 fileButton.pack(pady=15)
 
-
+# function that runs when widget is closed
 def on_quit():
+    # variables shared between threads
     global widget, exitFlag
     exitFlag = True
     widget.destroy()
 
-
+# sets up on_quit for when the widget exits
 widget.protocol("WM_DELETE_WINDOW", on_quit)
 
-
+# makes sure to connect to dispatcher before starting widget
 while(not connected):
     try:
         ws.connect(uri)
@@ -263,13 +292,17 @@ while(not connected):
         print('not connected: ' + str(e))
         time.sleep(1)
 
+# checks connection while the program is runnuing
 def checkConnection():
+    # variables shared between threads
     global connected, ws
     errorConnected = connected
+    # runs as long as the widget is running
     while not exitFlag:
         try:
             if not errorConnected:
                 try:
+                    # error reconncting
                     ws = None
                     ws = websocket.WebSocket()
                     ws.connect(uri)
@@ -277,6 +310,8 @@ def checkConnection():
                 except:
                     print('Connecting')
                     time.sleep(1)
+            # recieves messaged from dispatcher
+            ws.settimeout(40)
             check = ws.recv()
             if(check):
                 connected = True
@@ -295,7 +330,7 @@ def checkConnection():
             print('not connected: ' + str(e))
             time.sleep(1)
 
-
+# sets up dispatcher check thread
 dispatcherThread = threading.Thread(target=checkConnection)
 
 # starting the program
@@ -303,10 +338,11 @@ ws.send(json.dumps(register))
 dispatcherThread.start()
 voiceCommandThread.start()
 
+# starts the widget loop
 widget.mainloop()
 
 # exitting
-
+# joins threads
 voiceCommandThread.join()
 dispatcherThread.join()
 print('exit')
